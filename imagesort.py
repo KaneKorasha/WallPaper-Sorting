087d1478f1990935ddfe946344e022b06face2cb
KaneKorasha <Kane.Korasha@gmail.com>
2016-04-19 12:25:01 -0500

    Testing

--- imagesort.py
+++ imagesort.py
@@ -2,10 +2,6 @@ import os.path, shutil
 import get_image_size
 
 def main():
-    # Set directory to the picture source. This will eventually be 
-    # /home/aito/MainStorage/BTBox/Photos/PaperDump as it
-    # will be running on an Debian Server
-    # Rewrote to be pulled directly.
     directory = os.path.dirname(os.path.realpath(__file__))
     retrieveFile(directory)
 
