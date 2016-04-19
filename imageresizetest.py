8ccc26b00bb758887f832e421d84c0cceb907026
KaneKorasha <Kane.Korasha@gmail.com>
2016-04-19 16:04:47 -0500

    Back to working, with modules.
    Such a mess this is.

--- imageresizetest.py
+++ imageresizetest.py
@@ -4,14 +4,22 @@ import sys
 from PIL import Image
 
 def main():
-    directory = os.path.dirname(os.path.realpath(__file__))
+    directory = os.path.dirname(os.path.realpath('__file__'))
     retrieveFile(directory)
+
+def retrieveFile(directory):
+    for rawImage in os.listdir(directory):
+        if os.path.splitext(rawImage)[1].lower() in ('.jpg', '.jpeg', '.png'):
+            imagePil = Image.open(rawImage)
+            width, height = imagePil.size
+            imageCalculator(width, height, rawImage, imagePil)
+    return
+
+def imageCalculator(width, height, rawImage, imagePil):
     targetWidth = 1920
     targetHeight = 1080
-    targetRatio = float(targetWidth) / targetHeight
-    imagePil = Image.open('rawImage')
-    width, height = imagePil.size
     currentRatio = float(width) / height
+    targetRatio = float(targetWidth) / targetHeight
     if currentRatio == targetRatio:
         targetSize = targetWidth, targetHeight
     elif currentRatio > targetRatio:
@@ -22,18 +30,16 @@ def main():
         targetSize = targetWidth, newHeight
     else:
         print 'well shit.'
-    imageResize(targetSize, imagePil)
-    return
-def retrieveFile(directory):
-    for rawImage in os.listdir(directory):
-        if os.path.splitext(rawImage)[1].lower() in ('.jpg', '.jpeg', '.png'):
-    return
     
-def imageResize(targetSize, imagePil):
+    imageResize(targetSize, imagePil, rawImage)
+    
+    return
+        
+def imageResize(targetSize, imagePil, rawImage):
     imagePil.thumbnail(targetSize, Image.ANTIALIAS)
-    imagePil.save('rawImage')
+    imagePil.save(rawImage)
     return
-
-main()
     
+main()
+
 raw_input('Press Enter to Exit')
