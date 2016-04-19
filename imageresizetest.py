f43a5fac2e95dcfd177aa940d346d79e205045b0
KaneKorasha <Kane.Korasha@gmail.com>
2016-04-19 15:23:51 -0500

    ...

--- imageresizetest.py
+++ imageresizetest.py
@@ -4,10 +4,12 @@ import sys
 from PIL import Image
 
 def main():
+    directory = os.path.dirname(os.path.realpath(__file__))
+    retrieveFile(directory)
     targetWidth = 1920
     targetHeight = 1080
     targetRatio = float(targetWidth) / targetHeight
-    imagePil = Image.open('test.jpg')
+    imagePil = Image.open('rawImage')
     width, height = imagePil.size
     currentRatio = float(width) / height
     if currentRatio == targetRatio:
@@ -22,10 +24,14 @@ def main():
         print 'well shit.'
     imageResize(targetSize, imagePil)
     return
-
+def retrieveFile(directory):
+    for rawImage in os.listdir(directory):
+        if os.path.splitext(rawImage)[1].lower() in ('.jpg', '.jpeg', '.png'):
+    return
+    
 def imageResize(targetSize, imagePil):
     imagePil.thumbnail(targetSize, Image.ANTIALIAS)
-    imagePil.save('test.jpg')
+    imagePil.save('rawImage')
     return
 
 main()
